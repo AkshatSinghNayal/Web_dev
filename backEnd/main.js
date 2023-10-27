126ebb8b0613f82063df13b2ee7423c2bd8514bb
AkshatSinghNayal <programmer.akshat@gmail.com>
2023-10-27 23:12:07 +0530

    added some new files

---
+++ backEnd/main.js
@@ -0,0 +1,41 @@
+// const sum =(a,b)=>a+b;
+// const mul =(a,b)=>a*b;
+// const div =(a,b)=>a/b;
+// const pi = 3.14 ;
+// const G=9.8;
+
+// let obj = {
+//     jodna:sum,
+//     mulKarna:mul,
+//     pyiee:pi,
+//     G:G
+// }
+
+// module.exports=obj;
+
+
+// module.exports.sum =(a,b)=>a+b;
+// module.exports.mul =(a,b)=>a*b;
+// module.exports.div =(a,b)=>a/b;
+// module.exports.pi = 3.14 ;
+// module.exports.G=9.8;
+
+// module.exports = { WE DONT HAVE TO WRITE THIS IF WE ARE DEFINING THE FUNCTIONS BEFORE 
+//     jodna:sum,
+//     mulKarna:mul,
+//     pyiee:pi,
+//     G:G
+// }
+
+
+// exports.sum =(a,b)=>a+b; // THIS LINE OF CODE IS ALSO VALID     
+// exports.mul =(a,b)=>a*b;
+// exports.div =(a,b)=>a/b;
+// exports.pi = 3.14 ;
+// exports.G=9.8;
+
+export const sum =(a,b)=>a+b;
+export const mul =(a,b)=>a*b;
+export const div =(a,b)=>a/b;
+export const pi = 3.14 ;
+export const G=9.8;
