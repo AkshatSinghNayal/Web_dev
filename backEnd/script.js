c767422b3fd3341ca36715c53e1c727268746b59
AkshatSinghNayal <programmer.akshat@gmail.com>
2023-10-27 23:12:35 +0530

    added some new files

---
+++ backEnd/script.js
@@ -0,0 +1,26 @@
+// let n =2 ;
+
+// for(let i =1 ; i<=10;i++){
+//     console.log(`table is here ${i*n}`);
+// }
+
+// console.log("bye this is over now i am going to sleep ");
+
+
+// let arrName = process.argv;
+
+// for (let i = 2; i < arrName.length; i++) {
+//     console.log("hey you all ",arrName[i]);
+    
+// }
+
+// let ex = require("./main")
+// console.log(ex.jodna(10,2));
+
+// let ex = require("./main")
+// console.log(ex.sum(10,2));
+
+
+import { sum ,mul,div,pi,G } from "./main.js"
+    console.log(sum(2,3));
+    console.log(pi);
